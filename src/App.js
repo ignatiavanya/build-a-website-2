4dedec4c2d63ec3e2d880c4c6c3c35389e1b6fb5
Ignatia Vanya <ignatiavanya@gmail.comIgnatia04>
2020-12-29 11:00:44 +0700

    added a pict

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import './App.css';
+import img1 from './assets/img1.jpeg';
 
 function App() {
   return (
     <div className="App bg-blue-200 h-screen flex items-center justify-center">
       <header className="App-header">
-        <p className="text-2xl">
-          Edit <code>src/Home.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link text-sm font-light"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This is the weather-app starter code for Build a Website II
-        </a>
+        <img src={img1} />
+        <p>Hello</p>
       </header>
     </div>
   );
